c000a4f212633f254f780d827a340037257c5dcb
onnoo <sweyjw@gmail.com>
2020-06-03 09:39:29 +0900

    Server side API Call

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,13 +1,26 @@
-import {getAudio} from "../../lib/mellotron"
+// Backend
+import formidable from 'formidable';
+import FormData from 'form-data'
+import fs from 'fs'
 
-export default async (req, res) => {
-  const {filename, ok} = await getAudio(req.query)
-  if (ok) {
-    res.statusCode = 200
-    res.end(JSON.stringify({filename : filename}))
-  } else {
-    res.statusCode = 500
-    res.end(JSON.stringify({filename : "internel error"}))
-  }
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 }
 
+export default async (req, res) => {
+
+  const form = new formidable.IncomingForm();
+  form.uploadDir = "public/uploads";
+  form.keepExtensions = true;
+  form.on('file', function(field, file) {
+    console.log(`Upload ${file.name} to ${file.path}`)
+    res.statusCode = 200
+    res.end(JSON.stringify({filepath : file.path}))
+  })
+
+  form.parse(req, function(err, fields, files) {
+    console.log("done.")
+  })
+}
